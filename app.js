290508d8b74f1f1e7303208635f538bd71975134
Fábio Sakamoto <fabiosakamotoma@gmail.com>
2025-05-13 21:46:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,173 +1,112 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script>
 $(document).ready(function () {
-    var videos = [];
-    var dingsda;
-    var lastIds = [];
-    var position = 0;
-    $('#nextBtn').click(function () {
-        nextVideo();
-    });
-    $('#lastBtn').click(function () {
-        lastVideo();
-    });
+    const videos = [];
+    let dingsda;
+    const lastIds = [];
+    let position = 0;
 
-    if (position == 0) {
+    const playlistId = "PLeRB90AMH9_UheMdsk-DaUPzDMlqougvk";
+    const apiKey = "AIzaSyDaHEmQpkWJs_GkFjJuQoucV8TSJr4GT_k";
+
+    $('#nextBtn').on('click', nextVideo);
+    $('#lastBtn').on('click', lastVideo);
+
+    if (position === 0) {
         videoMove('out');
     } else {
         videoMove('in');
     }
 
-    var result = null;
-    $.ajax({
-        type: "GET",
-        url: "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&playlistId=PLeRB90AMH9_UheMdsk-DaUPzDMlqougvk&fields=items%2FcontentDetails%2FvideoId%2CnextPageToken&key=AIzaSyDaHEmQpkWJs_GkFjJuQoucV8TSJr4GT_k",
-        async: false,
-        success: function (data) {
-            result = data;
+    async function fetchVideos(pageToken = "") {
+        const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&playlistId=${playlistId}&key=${apiKey}${pageToken ? `&pageToken=${pageToken}` : ""}`;
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            data.items.forEach(item => videos.push(item.contentDetails.videoId));
+            if (data.nextPageToken) {
+                await fetchVideos(data.nextPageToken);
+            }
+        } catch (error) {
+            console.error("Erro ao buscar vídeos:", error);
         }
-    });
-
-    for (var i = 0; i < 50; i++) {
-        videos.push(result.items[i].contentDetails.videoId);
     }
-    nextOne(result.nextPageToken);
 
-    console.log("Amount: " + videos.length);
+    async function init() {
+        await fetchVideos();
+        console.log("Total de vídeos carregados:", videos.length);
 
-    var nbr = Math.floor((Math.random() * videos.length) + 1);
-    lastIds.push(videos[nbr]);
-    makeVideo(videos[nbr]);
+        const randomIndex = Math.floor(Math.random() * videos.length);
+        lastIds.push(videos[randomIndex]);
+        loadYouTubeAPI(() => makeVideo(videos[randomIndex]));
+    }
 
-    function nextOne(token) {
-        if (token) {
-            var json;
-            $.ajax({
-                type: "GET",
-                url: "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&pageToken=" + token + "&maxResults=50&playlistId=PLeRB90AMH9_UheMdsk-DaUPzDMlqougvk&fields=items%2FcontentDetails%2FvideoId%2CnextPageToken&key=AIzaSyDaHEmQpkWJs_GkFjJuQoucV8TSJr4GT_k",
-                async: false,
-                success: function (data) {
-                    json = data;
+    function loadYouTubeAPI(callback) {
+        const tag = document.createElement('script');
+        tag.src = "https://www.youtube.com/player_api";
+        window.onYouTubePlayerAPIReady = callback;
+        document.getElementsByTagName('script')[0].parentNode.insertBefore(tag, null);
+    }
+
+    function makeVideo(videoId) {
+        if (typeof dingsda !== 'undefined' && dingsda.loadVideoById) {
+            dingsda.loadVideoById(videoId);
+        } else {
+            dingsda = new YT.Player('output', {
+                width: $(window).width(),
+                height: $(window).height(),
+                videoId,
+                playerVars: {
+                    autoplay: position !== 0 ? 1 : 0,
+                    rel: 0,
+                    vq: 'large'
+                },
+                events: {
+                    onStateChange: onPlayerStateChange
                 }
             });
-            try {
-                for (var i = 0; i < 50; i++) {
-                    videos.push(json.items[i].contentDetails.videoId);
-                }
-                nextOne(json.nextPageToken);
-            } catch (err) {
-                console.log('No more Videos');
-            }
-        } else {
-            console.log('No more Videos');
         }
-    }
-
-    function makeVideo(id) {
-        if ($('#output').is('iframe')) {
-            dingsda.loadVideoById(id);
-        } else {
-            if (position == 0) {
-                var player;
-                window.onYouTubePlayerAPIReady = function () {
-                    player = new YT.Player('output', {
-                        width: $(window).width(),
-                        height: $(window).height(),
-                        videoId: id,
-                        playerVars: {
-                            rel: 0,
-                            vq: 'large'
-                        },
-                        events: {
-                            onStateChange: onPlayerStateChange
-                        }
-                    });
-                    dingsda = player;
-                }
-            } else {
-                var player;
-                window.onYouTubePlayerAPIReady = function () {
-                    player = new YT.Player('output', {
-                        width: $(window).width(),
-                        height: $(window).height(),
-                        videoId: id,
-                        playerVars: {
-                            autoplay: 1,
-                            rel: 0,
-                            vq: 'large'
-                        },
-                        events: {
-                            onStateChange: onPlayerStateChange
-                        }
-                    });
-                    dingsda = player;
-                }
-            }
-        }
-
-        function onPlayerStateChange(event) {
-            if (event.data === 0) {
-                nextVideo();
-            }
-        }
-        return;
     }
 
     function onPlayerStateChange(event) {
-        if (event.data === 0) {
+        if (event.data === YT.PlayerState.ENDED) {
             nextVideo();
         }
     }
 
-    var tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/player_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
     function nextVideo() {
-        if (position == 0) {
+        if (position === 0) {
             videoMove('in');
-            position += 1;
+            position++;
             dingsda.playVideo();
-            return false;
-        }
-        if (position < lastIds.length - 1) {
-            videoMove('in');
-            position += 1;
-            nextId = lastIds[position];
-            makeVideo(nextId);
+        } else if (position < lastIds.length - 1) {
+            position++;
+            makeVideo(lastIds[position]);
         } else {
-            videoMove('in');
-            var nbr = Math.floor((Math.random() * videos.length) + 1);
-            lastIds.push(videos[nbr]);
-            position += 1;
-            makeVideo(videos[nbr]);
+            const randomIndex = Math.floor(Math.random() * videos.length);
+            lastIds.push(videos[randomIndex]);
+            position++;
+            makeVideo(videos[randomIndex]);
         }
     }
 
     function lastVideo() {
         if (position > 1) {
-            position -= 1;
-            videoBefore = lastIds[position-1];
-            makeVideo(videoBefore);
-        } else {
-            if (position == 1) {
-                position -= 1;
-            }
+            position--;
+            makeVideo(lastIds[position - 1]);
+        } else if (position === 1) {
+            position--;
             videoMove('out');
             dingsda.pauseVideo();
         }
     }
 
-    function videoMove(way) {
-        if (way == 'in') {
-            console.log('Moved In');
-            $("#output").animate({
-                left: "0%",
-            }, 500);
-        } else if (way == 'out') {
-            console.log('Moved Out');
-            $("#output").animate({
-                left: "100%",
-            }, 500);
-        }
+    function videoMove(direction) {
+        const leftValue = direction === 'in' ? "0%" : "100%";
+        console.log(`Moved ${direction}`);
+        $("#output").animate({ left: leftValue }, 500);
     }
+
+    init(); // Start
 });
+</script>
